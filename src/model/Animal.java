546d1cde780843aaaf15231d40a954ed205be7c9
Julia <J.Schanda@gmx.at>
2021-04-06 20:37:28 +0200

    gemacht

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Animal {
+public class Animal implements Comparable<Animal> {
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "Name='" + Name + '\'' +
+                '}';
+    }
+
+    public String Name;
+
+
+
+    public Animal(String name) {
+        Name = name;
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        return Name.compareTo(o.Name);
+    }
 }
